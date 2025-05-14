c4cee050fe5e248e30d1fba5c4ab0f7221af698a
Isabele Martins <isabelemartinsnascimento15@gmail.com>
2025-05-14 00:41:56 -0400

    Introdução a Typescript

---
+++ Typescript/greeter.js
@@ -0,0 +1,8 @@
+var username = 'Davi';
+console.log(username);
+username = "ok";
+var a = 12;
+var b = '6';
+var c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ Typescript/greeter.ts
@@ -0,0 +1,12 @@
+let username : string = 'Davi'
+console.log(username)
+
+username = "ok"
+
+let a: number = 12
+let b: string = '6'
+let c: number = 2
+
+console.log(a / b)
+
+console.log(c * b)
