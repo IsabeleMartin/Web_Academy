4edec729c91c9b3ac116e231ba8ee15143f4281a
Isabele Martins <isabelemartinsnascimento15@gmail.com>
2025-06-26 16:29:27 -0400

    Exercício 7 de express

--- WACAD009-EXPRESS/ExpTS/src/index.ts
+++ WACAD009-EXPRESS/ExpTS/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import validateEnv from './utils/validateEnv';
 import router from './router/router';
 import dotenv from 'dotenv';
+import { engine } from 'express-handlebars'
 
 dotenv.config();
 validateEnv();
@@ -9,7 +10,13 @@ validateEnv();
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
 app.use(router);
+
+
 
 app.listen(PORT, () => {
   console.log(`Express app rodando na porta ${PORT}`);
--- WACAD009-EXPRESS/ExpTS/src/router/router.ts
+++ WACAD009-EXPRESS/ExpTS/src/router/router.ts
@@ -12,6 +12,13 @@ router.get("/post", (req, res) => {
 router.get("/sobre",(req,res) => {
     res.send("Bem-vindo à página sobre!")
 });
+
+// Serve arquivos estáticos de CSS, JS e imagens
+router.use('/css', express.static(`${publicPath}/css`));
+router.use('/js', express.static(`${publicPath}/js`));
+router.use('/img', express.static(`${publicPath}/img`));
+
+
 router.get( /^\/(api|rest)\/.+$/, (req, res) => {
     res.send("Envio de dados da API!");
 });
@@ -19,17 +26,13 @@ router.get("/bemvindo/:nome", (req, res) => {
     res.send(`Seja bem vindo ${req.params.nome}`);
 });
 
+
 // router para o exercício 5 de Express
 router.get('/lorem/:num', (req,res) =>{
     const num = Number(req.params.num);
     const text = loremmiddleware(num); 
     res.send(text);
 })
-
-// Serve arquivos estáticos de CSS, JS e imagens
-router.use('/css', express.static(`${publicPath}/css`));
-router.use('/js', express.static(`${publicPath}/js`));
-router.use('/img', express.static(`${publicPath}/img`));
 
 router.use(middleFunction);
 
@@ -40,5 +43,31 @@ router.post("/usuarios", (req: Request, res: Response) => {
 });
 
 
+// Rotas para o exercício 7 de express
+router.get('/hb1', (req, res) => {
+    res.render('hb1',{
+        mensagem: 'Olá, você está aprendendo Express + HBS!',
+        layout: false,
+    });
+});
+
+router.get('/hb2', (req, res) => {
+    res.render('hb2', {
+        poweredByNodejs: true,
+        name: 'Express',
+        type: 'Framework',
+        layout: false,
+    });
+});
+
+router.get('/hb3', (req, res) => {
+  const profes = [
+    { nome: 'David Fernandes', sala: 1238 },
+    { nome: 'Horácio Fernandes', sala: 1233 },
+    { nome: 'Edleno Moura', sala: 1236 },
+    { nome: 'Elaine Harada', sala: 1231 },
+  ];
+  res.render('hb3', { profes, layout: false });
+});
 
 export default router;
