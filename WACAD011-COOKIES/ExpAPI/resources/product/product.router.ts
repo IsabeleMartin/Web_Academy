3fdd4ec64aa45ea2f8971d653da0ba308563fa7f
Isabele Martins <isabelemartinsnascimento15@gmail.com>
2025-07-08 23:06:54 -0400

    Inicialização de cookies

---
+++ WACAD011-COOKIES/ExpAPI/resources/product/product.router.ts
@@ -0,0 +1,11 @@
+// Arquivo src/resources/product/product.router.ts
+import { Router } from 'express';
+import productController from './product.controller';
+const router = Router();
+// Product controller
+router.get('/', productController.index);
+router.post('/', productController.create);
+router.get('/:id', productController.read);
+router.put('/:id', productController.update);
+router.delete('/:id', productController.remove);
+export default router;
