74486666c3be09b65ed8b4f8d1a264e7c982ada9
Isabele Martins <isabelemartinsnascimento15@gmail.com>
2025-07-15 22:24:29 -0400

    Exercício I - Parte 1 Cookies

--- WACAD011-COOKIES/ExpAPI/resources/product/product.controller.ts
+++ WACAD011-COOKIES/ExpAPI/resources/product/product.controller.ts
@@ -1,20 +1,26 @@
-import {Response, Request } from 'express'
-const index = async (req: Request, res: Response) =>{
+import { Response, Request } from 'express'
+const index = async (req: Request, res: Response) => {
+    const produtos = [
+        { id: 1, name: "Notebook Acer", price: 1234 },
+        { id: 2, name: "Monitor Dell", price: 2000 },
+        { id: 3, name: "PC Gamer Dell", price: 4000 }
+    ];
+    res.json(produtos);
+}
+const create = async (req: Request, res: Response) => {
 
 }
-const create = async (req: Request, res: Response) =>{
+const read = async (req: Request, res: Response) => {
+    const productId = req.params.id;
+    res.end(productId)
+}
+const update = async (req: Request, res: Response) => {
 
 }
-const read = async (req: Request, res: Response) =>{
-
-}
-const update = async (req: Request, res: Response) =>{
-
-}
-const remove = async (req: Request, res: Response) =>{
+const remove = async (req: Request, res: Response) => {
 
 }
 
 
 
-export default {index, create, read, update, remove};
+export default { index, create, read, update, remove };
